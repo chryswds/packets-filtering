0dd6be5d0f613d78a79b75def4c190ce9ed3ff5e
chryswds <dybas.chrys@gmail.com>
2025-05-01 20:45:48 +0100

    receiving data

--- packet.c
+++ packet.c
@@ -11,8 +11,10 @@ int main(int argc, char *argv[])
 	struct sockaddr_in server;
 	char *ip_address = "172.217.23.110"; // googles ip address
 	int port = 80; //http port
-	char *message;
+	char *message, server_reply[2000];
 	
+
+	//CREATE SOCKET
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 	//Address Family - AF_INET (this is IP version 4) 
 	//Type - SOCK_STREAM (this means connection oriented TCP protocol) 
@@ -29,19 +31,30 @@ int main(int argc, char *argv[])
 	server.sin_port = htons(port);
 
 	
-	//Connect to remote server
+	//CONNECT TO REMOTE SERVER
 	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0){
 		printf("connect error");
 		close(socket_desc);
 		return 1;
 	}
 	printf("connected \n");
-	message = "GET / HTTP/1.1\r\n\r\n";
+
+	//SEND SOME DATA
+	message = "GET / HTTP/1.1\r\n\r\n";//The message is actually a http command to fetch the mainpage of a website.
 	if (send(socket_desc , message, strlen(message), 0) < 0){
 		printf("Send failed");
 		return 1;
 	}
 	printf("Data Send\n");
+
+
+	//RECEIVE A REPLY FROM THE SERVER
+	if( recv(socket_desc, server_reply, 2000, 0) < 0){
+		printf("recv failed");
+	}
+	printf("Reply received\n");
+	puts(server_reply);
+
 	return 0;
 
 };
